7f30ad0bb8b91cfd4040daf993f7828edd65c364
sato chiharu <kujira028@gmail.com>
2020-03-04 06:41:59 +0000

    完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,11 +32,11 @@ def edit
 end
 
 def update
-  book = Book.find(params[:id])
+  @book = Book.find(params[:id])
 
-  if book.update(book_params)
+  if @book.update(book_params)
 
-    redirect_to book_path(book.id),notice:"Book was successfully updated."
+    redirect_to book_path(@book.id),notice:"Book was successfully updated."
 
   else
     render action: :edit
